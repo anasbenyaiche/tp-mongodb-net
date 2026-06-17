ef9afd5e576b504ad95c2ce3d51ac4e2226b6563
anasbenyaiche <anas.benyaiche@yahoo.fr>
2026-06-17 12:06:06 +0200

    add mongo context

---
+++ bankflux/MongoContext.cs
@@ -0,0 +1,37 @@
+using BnakFlux.Models;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BnakFlux
+{
+    sealed class MongoContext
+    {
+        public IMongoClient Client { get; }
+        public IMongoDatabase Db { get; }
+
+        public MongoContext(string uri = "mongodb://localhost:27017",
+                    string dbName = "bankflux")
+        {
+
+            var pack = new ConventionPack
+        {
+            new CamelCaseElementNameConvention(),
+            new IgnoreExtraElementsConvention(true)
+        };
+            ConventionRegistry.Register("bankflux", pack, _ => true);
+
+            Client = new MongoClient(uri);
+            Db = Client.GetDatabase(dbName);
+        }
+        public IMongoCollection<Customer> Customers => Db.GetCollection<Customer>("customers");
+        public IMongoCollection<Account> Accounts => Db.GetCollection<Account>("accounts");
+        public IMongoCollection<Transaction> Transactions => Db.GetCollection<Transaction>("transactions");
+        public IMongoCollection<Branch> Branches => Db.GetCollection<Branch>("branches");
+
+    } 
+}
